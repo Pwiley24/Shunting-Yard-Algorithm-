5a88c60e716c9c4ba996e89b89aa492d9e03a2ea
Pwiley24 <paigewiley24@icloud.com>
2023-03-19 18:58:38 -0700

    tree doesn't work

--- main.cpp
+++ main.cpp
@@ -8,7 +8,8 @@
 using namespace std;
 
 void postfixNotation(Stack* stack, Queue* queue, vector<char> &postfix);
-void createTree(Stack* &tree, vector<char> postfix);
+void createTreeList(vector<char> &tree, vector<char> postfix);
+void printInfixNotation(vector<char> tree);
 
 int main(){
   char input[20];
@@ -17,7 +18,7 @@ int main(){
   //Stack List:
   Queue* queue = new Queue();
   Stack* stack = new Stack(queue);
-  Stack* tree = new Stack(NULL); 
+  vector<char> tree; 
   vector<char> postfix;
 
   //Queue List:
@@ -57,25 +58,69 @@ int main(){
 
 
   postfixNotation(stack, queue, postfix);
-  createTree(tree, postfix);   
+  createTreeList(tree, postfix);   
+  printInfixNotation(tree);
   
 }
 
-void createTree(Stack* &tree, vector<char> postfix){
+void createTreeList(vector<char> &tree, vector<char> postfix){
   vector<char>::iterator ptr;
-  for(ptr = postfix.begin(); ptr < postfix.end(); ptr++){
-    Node* newNode = new Node();
-    newNode->setValue(*ptr);
-    
+  for(ptr = postfix.end(); ptr >= postfix.begin(); ptr--){//iterate backward
+    cout << *ptr << endl;
+    tree.push_back(*ptr); 
   }
 }
+
+//print left child, parent, right child for all
+void printInfixNotation(vector<char> tree){
+  vector<char> infixNotation;
+  char child2 = NULL;
+  char optor = NULL;
+  char child1 = NULL;
+  vector<char>::iterator ptr;
+  for(ptr = tree.end(); ptr > tree.begin(); ptr--){
+    cout << "now " << *ptr << endl;
+    if(*ptr >= 48 &&
+       *ptr <= 57 &&
+       child1 == NULL){//start of new operation
+     cout << "first child" << endl;
+      infixNotation.push_back('(');
+      infixNotation.push_back(*ptr);
+      child1 = *ptr;
+    }else if(*ptr >= 48 &&
+             *ptr <= 57 &&
+             child2 == NULL){//other child value going through
+      cout << "child2 " << *ptr << endl;
+      child2 = *ptr;
+    }else if(optor == NULL){//parent value going through
+      optor = *ptr;
+      cout << "optor " << optor << endl;
+      infixNotation.push_back(optor);
+      infixNotation.push_back(child2);//operand
+      infixNotation.push_back(')');
+      child1 = NULL;
+      optor = NULL;
+      child2 = NULL;
+    }   
+  }
+  for(ptr = infixNotation.begin(); ptr < infixNotation.end(); ptr++){
+    cout << *ptr;
+  }
+  cout << " infix " << endl;
+}
+
 
 void postfixNotation(Stack* stack, Queue* queue, vector<char> &postfix){
   while(queue->peek() != NULL){
     postfix.push_back(queue->dequeue()->getValue());
   }
   while(stack->peek() != NULL){
-    postfix.push_back(stack->pop()->getValue());
+    if(stack->peek()->getValue() != 41 &&
+       stack->peek()->getValue() != 40){
+      postfix.push_back(stack->pop()->getValue());
+    }else{
+      stack->pop();
+    }
   }
 
   vector<char>::iterator ptr;
